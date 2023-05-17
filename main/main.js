82c2b8b01025c521452d905f01055aea0b1c4e99
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-05-17 16:49:32 -0500

    feat: :sparkles: funcionalidad del video
    agregue que mediante un api el buscador me trajera el array y muestre el primer video del array

---
+++ main/main.js
@@ -0,0 +1,22 @@
+let buscador = document.querySelector("#buscador1")
+let boton = document.querySelector("#lupa1")
+let videoYutu = document.querySelector("#video1")
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '143e44ebc7msh671913b0fd5416fp142749jsnb8dcca7ba67e',
+		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+	}
+};
+boton.addEventListener("click",(e)=>{
+    e.preventDefault()
+    buscarDatos(buscador.value)
+})
+async function buscarDatos(id){
+    console.log(id)
+    let url = `https://youtube138.p.rapidapi.com/search/?q=${id}&hl=en&gl=US`;
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result.contents[0].video.videoId);
+    videoYutu.setAttribute("src", `https://www.youtube.com/embed/${result.contents[0].video.videoId}`)
+}
