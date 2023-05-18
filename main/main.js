71da22d340172c7fc5fb15ffaa4d50c3dc99b62b
lvillamizarmurillo <lvillamizarmurillo@gmail.com>
2023-05-18 17:00:46 -0500

    feat: :sparkles: Comentarios, logo, titulo
    Se agregaron en sus respectivas cajas la imagen del canal con el nombre del mismo, aparte se le agregan todos los comentarios con un scroll y ya se diseño con css para que se vea bonito :)

--- main/main.js
+++ main/main.js
@@ -1,6 +1,10 @@
 let buscador = document.querySelector("#buscador1")
 let boton = document.querySelector("#lupa1")
 let videoYutu = document.querySelector("#video1")
+let canalYutuLogo = document.querySelector("#logoCanal1")
+let nombreCanlal = document.querySelector("#nombreCanal1")
+let tituloVideo = document.querySelector("#titulo1")
+let comentariosYutu = document.querySelector("#comentariosYutu")
 const options = {
 	method: 'GET',
 	headers: {
@@ -13,10 +17,30 @@ boton.addEventListener("click",(e)=>{
     buscarDatos(buscador.value)
 })
 async function buscarDatos(id){
-    console.log(id)
     let url = `https://youtube138.p.rapidapi.com/search/?q=${id}&hl=en&gl=US`;
     const response = await fetch(url, options);
     const result = await response.json();
-    console.log(result);
     videoYutu.setAttribute("src", `https://www.youtube.com/embed/${result.contents[0].video.videoId}`)
+    tituloVideo.innerHTML = result.contents[0].video.title
+    canalYutu(result.contents[0])
+    comentariosYutu1(result.contents[0].video.videoId)
+}
+async function canalYutu(res){
+    canalYutuLogo.setAttribute("src", res.video.author.avatar[0].url)
+    nombreCanlal.innerHTML = res.video.author.title
+}
+async function comentariosYutu1(videoId){
+    let url = `https://youtube138.p.rapidapi.com/video/comments/?id=${videoId}&hl=en&gl=US`;
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+    comentariosYutu.innerHTML = ``
+    for(let i=0;i<result.comments.length;i++){
+        comentariosYutu.innerHTML += `
+        <div class="comenta">
+            <h4>${result.comments[i].author.title}</h4>
+            <p>${result.comments[i].content}</p>
+        </div>
+        `
+    }
 }
